f204f3bd0e3de9e165d071cc8861e9885cc9f9b8
Yura Jo <yur8lnne@gmail.com>
2023-07-17 13:49:08 -0500

    Second

---
+++ final project/twiiter.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from tqdm.notebook import tqdm
+import snscrape.modules.twitter as sntwitter
+
+sntwitter.TwitterSearchScraper("#coding")
+tweets =[]
+n_tweets =1000
+
+for i,tweet in tqdm(enumerate(scraper.get_items())):
+    data =[
+        tweet.date,
+        tweet.id,
+        tweet.content,
+        tweet.user.username,
+        tweet.likeCount,
+        tweet.retweetCount,
+    ]
+    tweets.append(data)
+    if i>n_tweets:
+        break
+
+tweet_df =pd.DataFrame(
+    tweets, columns=["date","id","content","username","like_count","retweet_count"]
+)
+
+tweet_df.to_csv("tweets.csv", index=False)
--- final project/twint.py
+++ final project/twint.py
@@ -1,2 +1,1 @@
-pip3 install ––user ––upgrade git+https://github.com/twintproject/twint.git@origin/master#egg=twint
-cs
+import 
